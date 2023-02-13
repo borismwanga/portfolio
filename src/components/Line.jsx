4343e3fef65a77829e34e6cf09a549e8ed72e16c
Boris Mwnaga <borismwanga@gmail.com>
2023-02-13 16:27:30 +0100

    setup

---
+++ src/components/Line.jsx
@@ -0,0 +1,7 @@
+export default function Line(){
+    return(
+        <div class="line">
+            <h1>B</h1>
+        </div>
+        )
+}
---
+++ src/components/boxmenu.jsx
@@ -0,0 +1,13 @@
+export default function Boxmenu(){
+    return(
+        <div className="container">
+             <div className="grid">
+            <div className="div1"> Contact</div>
+            <div className="div2"> Aboutd</div>
+            <div className="div3"> Abouddt</div>
+            <div className="div4">projects</div>
+        </div>
+        </div>
+        
+    )
+}
