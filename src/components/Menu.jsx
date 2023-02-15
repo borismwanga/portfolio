f5630f746b384ab0995c41f403d1c0074c581483
Boris Mwnaga <borismwanga@gmail.com>
2023-02-15 13:21:34 +0100

    animation and more

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,19 +2,32 @@ import { gsap } from "gsap";
 import { useState } from "react";
 
 
-export default function Menu(){
+export default function Menu(props){
+
+    const { handleClick, activeComponent } = props;
 
     const menuClick = () => {
-        gsap.to(".container",{x:-400, y:-100, scale:0.5, duration:1})
+        gsap.to(".container",{x:-450, y:-100, scale:0.5, duration:0.5})
       }
 
     return(
         <div className="container" onClick={menuClick}>
             <div className="grid">
-                <div className="div1"> CONTACTS</div>
-                <div className="div2"> ABOUT</div>
+                <div className={`div1 ${activeComponent === "target1" ? "active" : ""}`}
+                    onClick={() => handleClick("target1")}> 
+                    CONTACTS
+                </div>
+
+                <div className={`div2 ${activeComponent === "target2" ? "active" : ""}`}
+                    onClick={() => handleClick("target2")}> 
+                    ABOUT
+                </div>
                 <div className="div3"> PROFIL</div>
-                <div className="div4"> PROJECT</div>
+
+                <div className={`div4 ${activeComponent === "target4" ? "active" : ""}`}
+                    onClick={() => handleClick("target4")}>
+                    PROJECT
+                </div>
             </div>
         </div>
         
