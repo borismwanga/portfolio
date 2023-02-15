053f1063bccd2f70d8eeb2b3d39bcb2fefe27958
Boris Mwnaga <borismwanga@gmail.com>
2023-02-15 11:20:49 +0100

    almost done

---
+++ src/data.js
@@ -0,0 +1,33 @@
+export default [
+
+    {
+        id: 1,
+        title:"Chillwheel",
+        description:"Chillwheel is a web application that allows users to view, report, and search for bike accidents and stolen bikes in their area. It is built using the Ruby on Rails framework and offers a map view to display incidents, a search and filter function to find specific incidents, and a report form to add new incidents. Users can also use the contact form to get in touch with the owner of a stolen bike if they have information that can help with its recovery.",
+        image: "https://live.staticflickr.com/65535/52688932979_7b42c01e4d_k.jpg",
+        link: "https://www.chillwheel.cc",
+        github: "https://github.com/borismwanga/chillwheel"
+
+    },
+
+    {
+        id: 2,
+        title:"Logos Mwanga Portfolio",
+        description:"On this website, you'll find a selection of Logos' design and illustration work. Logos is a skilled and creative designer with a passion for creating unique designs. In the portfolio section, you'll find logos, branding designs, book covers, and digital illustrations. Each project showcases Logos' attention to detail and ability to bring clients' visions to life.",
+        image: "https://live.staticflickr.com/65535/52688934044_5b82014b01_k.jpg",
+        link: "https://www.logosmwanga.com",
+        github: "https://github.com/borismwanga/logosmwanga"
+
+    },
+
+    {
+        id: 3,
+        title:"NextDive",
+        description:"Chillwheel is a web application that allows users to view, report, and search for bike accidents and stolen bikes in their area. It is built using the Ruby on Rails framework and offers a map view to display incidents, a search and filter function to find specific incidents, and a report form to add new incidents. Users can also use the contact form to get in touch with the owner of a stolen bike if they have information that can help with its recovery.",
+        image: "https://live.staticflickr.com/65535/52689083375_bffb08e4a2_k.jpg",
+        link: "https://www.nextdive.xyz/",
+        github: "https://github.com/lau2424"
+
+    }
+
+]
