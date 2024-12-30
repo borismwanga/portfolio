8a165b6e66fe95e7fd28c06c49c6e8811c535256
borismwanga <57875197+borismwanga@users.noreply.github.com>
2024-12-31 00:31:35 +0100

    about, project, contact

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['images.unsplash.com'],
+    domains: [
+      'images.unsplash.com',
+      'res.cloudinary.com',
+      'cdn.sanity.io',
+      'live.staticflickr.com'
+    ],
   },
 };
 
--- src/components/main/About.tsx
+++ src/components/main/About.tsx
@@ -7,13 +7,13 @@ import CirclingElements from "@/components/fancy/blocks/circling-elements"
 
 export const exampleImages = [
     "/images/image.jpeg",
-    "https://images.unsplash.com/photo-1640680608781-2e4199dd1579?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    "https://images.unsplash.com/photo-1726083085160-feeb4e1e5b00?q=80&w=3024&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    "https://images.unsplash.com/photo-1725955278540-bc756fbcd0a5?q=80&w=2592&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1556962021-9d0303621643?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     "https://images.unsplash.com/photo-1624344965199-ed40391d20f2?q=80&w=2960&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    "https://images.unsplash.com/photo-1689553079282-45df1b35741b?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1487887235947-a955ef187fcc?q=80&w=1955&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "/images/image_2.jpg",
     "https://images.unsplash.com/photo-1721968317938-cf8c60fccd1a?q=80&w=2728&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     "https://images.unsplash.com/photo-1677338354108-223e807fb1bd?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1580894906475-403276d3942d?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   ]
 
 const About: React.FC = () => {
@@ -21,6 +21,12 @@ const About: React.FC = () => {
 
   return (
     <div className="w-full h-full  flex items-center justify-center">
+        <span className="z-10 text-center text-4xl font-appelGarmondLight w-1/2 leading-8">
+            Hi there! I amm Boris Mwanga, a passionate developer with a love for coding, problem-solving, and 
+            creating impactful websites and applications. Beyond my tech journey, I find inspiration in music, 
+            football, and the vibrant stories they bring to life. I am always curious and eager to explore new technologies, 
+            delve into creative coding, and expand my horizons. Let&apos;s build something amazing together!
+        </span>
       <CirclingElements radius={screenSize.lessThan(`md`) ? 80 : 120}>
         {exampleImages.map((url, index) => (
           <div
@@ -31,7 +37,7 @@ const About: React.FC = () => {
           </div>
         ))}
       </CirclingElements>
-      thissfsfsfsfsfs
+     
     </div>
   )
 }
